635285920768764b8cc1aaa3a341534a284c5918
DiogoCardinot <diogocardinotuerj@gmail.com>
2025-03-28 18:26:10 -0300

    Faz a tela inicial e o consumo de dados com API

--- BackPythonWeb/aula/views.py
+++ BackPythonWeb/aula/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from .models import Aluno
+import requests
 
-# Create your views here.
+def Home(request):
+    return render(request, 'aula/aula.html')
+
+# Acesso aos dados da base de dados
+def ConectaBaseDados(request):
+    alunos = Aluno.objects.all()
+    context={
+        'alunos' : alunos,
+    }
+
+    return render(request, 'aula/alunos.html', context)
+
+# Conexão e consumo de dados com API
+def ConsumirAPI(request):
+    url = 'https://jsonplaceholder.typicode.com/users'
+    resposta = requests.get(url)
+    usuariosAPI = resposta.json() 
+    context = {
+        'usuariosAPI': usuariosAPI
+    }
+    return render(request, 'aula/ConsumirAPI.html', context)
